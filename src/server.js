91266550e3c33ceafdeb2ba38c101b9a3d4be91c
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2024-06-20 20:00:57 +1000

    feat: blog router skeleton created

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
+
+
+
 // Allows POST requests to have JSON body content
 app.use(express.json());
+
+
 
 app.get("/", (request, response, next) => {
 
@@ -10,6 +15,12 @@ app.get("/", (request, response, next) => {
 		message: "Hello world!"
 	});
 });
+
+
+
+const blogRouter = require("./controllers/BlogRouter.js");
+app.use("/blogs", blogRouter);
+
 
 
 app.get("*", (request, response, next) => {
@@ -28,5 +39,7 @@ app.use((error, request, response, next) => {
 
 
 module.exports = {
-    app
-};
+	app
+}
+
+
